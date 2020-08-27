7c9304ae1f5c1610dda3b1d5ce2990f2733150c6
Laurent Leturgez <leturgezl@gmail.com>
2020-08-27 14:35:28 +0200

    Update and rename distrib_example.py to full_example.py

--- SparkPerformanceAndPartitioningStrategies/full_example.py
+++ SparkPerformanceAndPartitioningStrategies/full_example.py
@@ -1,5 +1,7 @@
 import random as rand
 import pandas as pd
+from matplotlib import pyplot as plt
+from matplotlib import cm
 
 def newDf(data,values_cnt=50,partition_cnt=4):
   src=[]
@@ -61,4 +63,24 @@ def df_details_pd(df):
     part_num=part_num+1
   return res
   
-  
+if __name__== "__main__":
+  names=('aaaaaaaaaa','bbbbbbbbbb',
+       'cccccccccc','dddddddddd',
+       'eeeeeeeeee','ffffffffff',
+       'ggggggggggg','hhhhhhhhhh')
+  nb_of_values=1000000
+  nb_part=8
+
+
+  df=newDf(data=names,
+           values_cnt=nb_of_values,
+           partition_cnt=nb_part)
+
+  pddf_res=df_details_pd(df)
+          
+  cmap = cm.get_cmap('tab20') 
+
+  fig,ax=plt.subplots(2,1,figsize=(20,30))
+  pddf_res.plot(kind='bar',x='value', rot=0, figsize=(20,15), ax=ax[0])
+  pddf_res.plot(kind='pie', y='cnt', ax=ax[1], colormap=cmap)
+  ax[1].legend( pddf_res.value, loc='lower center', ncol=len(pddf_res.value))
